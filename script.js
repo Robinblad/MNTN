c6667c2dcf294090907a8f90f4e29ce691544958
Robinblad <0YjhN9givIhINI2SMDkp7cl8k7FhG9lz@protonmail.com>
2024-03-25 23:37:29 +0600

    Add css styles for small gadgets

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.getElementById("body").onscroll = function myFunction() {  
-    var scrolltotop = document.scrollingElement.scrollTop;
-    var target = document.getElementById("imgBackground");
-    var xvalue = "center";
-    var factor = 0.5;
-    var yvalue = scrolltotop * factor;
-    target.style.backgroundPosition = xvalue + " " + yvalue + "px";
-}
+document.getElementById("body").onscroll = function myFunction() {
+	var scrolltotop = document.scrollingElement.scrollTop;
+	var target = document.getElementById("imgBackground");
+	var xvalue = "center";
+	var factor = 0.5;
+	var yvalue = scrolltotop * factor;
+	target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+};
